2f2eda7146d00e35c2b77064a07dfffb49dad71c
BisalBosu <bishalbosu@gmail.com>
2023-03-06 21:02:12 +0530

    with try catch block and async and await

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //crudcrud base url
-const url = "https://crudcrud.com/api/a26a92fa0cf642c5a0b89f377de57d6d";
+const url = "https://crudcrud.com/api/9934956adff040cd8b046d2ee2d252a6";
 
 
-function clicked(){
+async function clicked(){
     let price = document.getElementById("price").value;
     let name = document.getElementById("name").value;
     let catagoty = document.getElementById("type").value;
@@ -14,104 +14,52 @@ function clicked(){
     };
 
 
-    if (catagoty=="electronics"){
-        axios
-        .post(url+"/electronics", obj)
-        .then(r=>{
-            element = r.data;
-            document.getElementById("electronics").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        })
-        .catch(e => {
-            console.log(e);
-        })
-    }
-    else if (catagoty=="food"){
-        axios
-        .post(url+"/food", obj)
-        .then(r=>{
-            element = r.data;
-            document.getElementById("food").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        })
-        .catch(e => {
-            console.log(e);
-        })
-    }
-    else if (catagoty=="skincare"){
-        axios
-        .post(url+"/skincare", obj)
-        .then(r=>{
-            element = r.data;
-            document.getElementById("skin").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        })
-        .catch(e => {
-            console.log(e);
-        })
-    }
+    clickProcess(catagoty, obj);
    
 }
 
+async function clickProcess(type, obj){
+    try{
+        const r = await axios
+        .post(url+`/${type}`, obj);
 
+        const element = await r.data;
+        
+        document.getElementById(`${type}`).innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
+        <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
+        
+    }
+    catch(e) {
+        console.log(e);
+    }
+
+}
 
 
 function show(){
     //get for electro
-    axios
-    .get(url+"/electronics")
-    .then(r => {
-        r.data.forEach(element => {
-            document.getElementById("electronics").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        });
-    })
-    .catch(e=>{
-        console.log(e);
-    })
+    showProcess("electronics");
 
 
     //get for food
-    axios
-    .get(url+"/food")
-    .then(r => {
-        r.data.forEach(element => {
-            document.getElementById("food").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        });
-    })
-    .catch(e=>{
-        console.log(e);
-    })
+    showProcess("food");
 
     //get for skincare
-    axios
-    .get(url+"/skincare")
-    .then(r => {
-        r.data.forEach(element => {
-            document.getElementById("skin").innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
-            <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
-
-        });
-    })
-    .catch(e=>{
-        console.log(e);
-    })
+    showProcess("skincare");
 }
 
-function del(id, catagory){
-    
-axios
-    .delete(url+`/${catagory}/${id}`)
-    .then(document.getElementById(id).remove())
-    .catch(err =>{
-        console.log(err);
-    })
+//delete function
+async function del(id, catagory){ 
+    try{
+        const r = await axios
+            .delete(url+`/${catagory}/${id}`);
+
+        document.getElementById(id).remove();
+    }
+    catch(err){
+
+            console.log(err);
+        }
 
     
 }
@@ -119,5 +67,25 @@ axios
 window.addEventListener("DOMContentLoaded", () =>{
     show();
 
-
 })
+
+async function showProcess(type){
+    try{
+        const r = await axios
+        .get(url+`/${type}`);
+
+        const data = await r.data;
+
+        
+        data.forEach(element => {
+                document.getElementById(`${type}`).innerHTML += `<div id = "${element._id}" class="mb-3"> &#8377;${element.price} - ${element.catagoty} - ${element.name} 
+                <button type="button" onclick="del('${element._id}', '${element.catagoty}')" class="btn btn-danger"> Delete product</button> </div>`
+
+            });
+        
+    }
+    catch(e){
+        console.log(e);
+    }
+
+}
